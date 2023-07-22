a95910a5f3392f4522c1a8a426d1e207404b3ac7
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-07-22 03:07:41 +0000

    Started routing

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,9 +5,11 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { Planets } from "./views/planets";
+import { Characters } from "./views/characters";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { Navbar } from "./views/navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -24,6 +26,8 @@ const Layout = () => {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
+						<Route path="/planets" element={<Planets/>}/>
+						<Route path="/characters" element={<Characters/>}/>
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
